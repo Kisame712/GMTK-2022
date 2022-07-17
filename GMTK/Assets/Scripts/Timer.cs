e7283a905900df144fd1905b83a52abcdd27ace2
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2022-07-17 22:40:11 +0530

    panels added

--- GMTK/Assets/Scripts/Timer.cs
+++ GMTK/Assets/Scripts/Timer.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public GameObject[] panels = new GameObject[6] ;
     public Text timerText;
     int timerTime = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        panels[0].SetActive(false);
+        panels[1].SetActive(false);
+        panels[2].SetActive(false);
+        panels[3].SetActive(false);
+        panels[4].SetActive(false);
+        panels[5].SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -28,6 +35,7 @@ public class Timer : MonoBehaviour
     void DiceThrow()
     {
         int randomThrow = Random.Range(1, 7);
+        panels[randomThrow - 1].SetActive(true);
 
     }
 }
